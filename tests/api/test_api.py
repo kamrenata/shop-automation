7df0a174ee437c581578e96e5cd5e993a89f6cde
Renata Kamalova <renatakamal@gmail.com>
2024-03-05 12:03:19 +0400

    Add checks for api tests

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -3,4 +3,11 @@ import requests
 
 def test_get_facts_request():
     response = requests.get("https://cat-fact.herokuapp.com/facts/", headers={"Accept": "application/json"})
+    a = response.json()
     assert response.json()[0]["text"]
+    for x in a:
+        assert "_id" in x
+        assert isinstance(x["type"], str)
+        assert isinstance(x["deleted"], bool)
+        assert isinstance(x["__v"], int)
+
